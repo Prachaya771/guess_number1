e9bee1e0dc74a7cafaff3e5a1bda96a1f4327eed
Prachaya Suwannasuk <suwannasuk_p@silpakorn.edu>
2021-12-12 13:26:16 +0700

    commit

---
+++ lib/test.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  var i = 0;
+  var r = Random();
+  dynamic c;
+
+  var answer = r.nextInt(10);
+
+
+  do {
+    stdout.write('Please guess the number : ');
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+    if (guess == answer) {
+      c = guess;
+      print('you guessed right');
+      break;
+    } else {
+      i++;
+      if(i==10)
+        print('You guessed wrong, try again');
+    }
+
+  } while(i<10);
+  print('Random Number : ${answer}'); //หาวิธี print จากในเน็ต
+  print('End The Program');
+}
